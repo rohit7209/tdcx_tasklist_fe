75aa607d622c0fd7fe4286564182b1d634091d82
Rohit Sharma <rohit@numberzs-MacBook-Pro.local>
2021-06-14 09:59:12 +0530

    base url added

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 export const apiConstants = {
-  BASE_URL: 'http://localhost:8080',
+  BASE_URL: 'https://tdcx-tasklist-be.herokuapp.com',
+  // BASE_URL: process.env.BASE_URL || 'http://localhost:8080',
   LOGIN: '/login',
   DASHBOARD: '/dashboard',
   TASKS: '/tasks',
